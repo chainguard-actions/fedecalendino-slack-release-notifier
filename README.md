88d0b6d2022536410fe1addb2add0127e8322632
actionchain <actionchain@chainguard.dev>
2026-05-11 17:30:21 +0000

    Update fedecalendino/slack-release-notifier@v3.0.2

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # slack-release-notifier
+
+Simple action to notify a slack channel after a new version of a project has been released.
+
+### Requirements
+
+To be able to send the notification, a webhook url needs to be provided. You can get one by installing the app [Incoming WebHooks](https://slack.com/apps/A0F7XDUAZ-incoming-webhooks) into your slack workspace.
+
+
+## Default Usage
+
+![default configuration](/img/custom.png)
+
+workflow.yml
+```yml
+name: Release
+
+on:
+  release:
+    types:
+      - created
+
+jobs:
+  publish:
+    runs-on: ubuntu-latest
+    timeout-minutes: 5
+
+    steps:
+    - name: Notify Slack
+      id: slack
+      with:
+        slack_webhook_url: ${{ secrets.SLACK_WEBHOOK_URL }}
+      uses: fedecalendino/slack-release-notifier@v3.0.2
+```
+
+## Custom Usage
+
+![custom configuration](/img/custom.png)
+
+workflow.yml
+```yml
+name: Release
+
+on:
+  release:
+    types:
+      - created
+
+jobs:
+  publish:
+    runs-on: ubuntu-latest
+    timeout-minutes: 5
+
+    steps:
+    - name: Notify Slack
+      id: slack
+      with:
+        slack_webhook_url: ${{ secrets.SLACK_WEBHOOK_URL }}
+        slack_button_text: "open in pypi"
+        slack_button_url: "https://pypi.com"
+        slack_button_emoji: ":pypi:"
+      uses: fedecalendino/slack-release-notifier@v3.0.2
+```
+
+
+
+## Privacy
+
+This Action contacts Chainguard's licensing server to verify authorization. Connection metadata (IP address, GitHub repository identifier, timestamp, and any metadata encoded in the auth token) is transmitted to Chainguard, Inc. even if authorization is denied in accordance with our [Privacy Notice](https://www.chainguard.dev/legal/privacy-notice)
---
+++ main.py
@@ -0,0 +1,76 @@
+from uuid import uuid4
+
+import urllib.request
+import json
+from slackblocks import (
+    WebhookMessage,
+    SectionBlock,
+    Button,
+    ContextBlock,
+    Image,
+    Text,
+    ResponseType,
+)
+
+from actions import GITHUB, PARAMS
+
+
+def main():
+    title = f"*{GITHUB.name.upper()}*: new version _<{GITHUB.url}|{GITHUB.version}>_ was released!"
+    preview = f"{GITHUB.name.upper()}: new version {GITHUB.version} was released by {GITHUB.user_name}!"
+
+    message = WebhookMessage(
+        response_type=ResponseType.IN_CHANNEL,
+        text=preview,
+        blocks=[
+            SectionBlock(
+                text=title,
+                accessory=Button(
+                    text=f":{PARAMS.button_emoji}: {PARAMS.button_text}",
+                    url=PARAMS.button_url,
+                    action_id=str(uuid4()),
+                ),
+            ),
+            ContextBlock(
+                elements=[
+                    Image(
+                        image_url=GITHUB.user_image_url,
+                    ),
+                    Text(
+                        text=f"by {GITHUB.user_name}",
+                    ),
+                ]
+            ),
+        ],
+    )
+
+    send(PARAMS.slack_webhook_url, message)
+
+
+def send(url: str, message: WebhookMessage):
+    data = message.json().encode("utf-8")
+
+    req = urllib.request.Request(
+        url,
+        data=data,
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+
+    try:
+        with urllib.request.urlopen(req) as response:
+            status_code = response.getcode()
+
+            if status_code != 200:
+                response = response.read().decode("utf-8")
+
+                raise Exception(f"Failed to send message to Slack > {status_code} {response}")
+            else:
+                print(f"Message was sent successfully > {message.text}")
+    except urllib.error.HTTPError as e:
+        raise Exception(f"Failed to send message to Slack > {e.code} {e.reason}")
+
+
+if __name__ == "__main__":
+    main()
